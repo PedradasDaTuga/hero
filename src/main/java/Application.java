6fbb945a36141f531e5b1ac2b9be7e6ee13790c3
Pedro Correia <pedromiguelcorreia2002@gmail.com>
2021-11-06 22:39:32 +0000

    Classe Game adicionada que cria terminal

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -14,23 +14,7 @@ import java.util.EnumSet;
 
 public class Application {
     public static void main(String[] args) throws IOException {
-        try {
-         //   Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            TerminalSize terminalSize = new TerminalSize(40, 20);
-            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
-            Terminal terminal = terminalFactory.createTerminal();
-            Screen screen = new TerminalScreen(terminal);
-            screen.setCursorPosition(null);
-            screen.startScreen();
-            screen.doResizeIfNecessary();
-            screen.clear();
-            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
-            screen.refresh();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-
+    Game game= new Game();
+    game.run();
     }
 }
